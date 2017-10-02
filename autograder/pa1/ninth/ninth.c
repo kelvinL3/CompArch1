d3c291aeb13e64c6be787aec35b2d1296daf284f
Kelvin <kelvinliu1234@gmail.com>
2017-10-02 15:46:02 -0400

    try

--- autograder/pa1/ninth/ninth.c
+++ autograder/pa1/ninth/ninth.c
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
 				printf("absent\n");
 			}
 		} else if (instruction == 'd') {
-			root = deleteFromTree(root, data);
+			root = helperDelete(root, data);
 		}
 	}
 	
