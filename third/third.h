b161c710b16c7c891b104fb48dd351e52ed8d833
Kelvin <kelvinliu1234@gmail.com>
2017-10-02 02:49:42 -0400

    third

--- third/third.h
+++ third/third.h
@@ -6,17 +6,17 @@ const unsigned int bucketNumber = 10000;
 struct node {
 	struct node *next;
 	int data;
-}
+};
 
 //functions
 //returns modulus
 int hash(int query, int size);
 
 //go to position in array, go down linked list until end
-int insert(int *array, int position);
+int insert(int **array, int position);
 
 //go to position, search through linked list until finding query
-int search(int *array, int position, int query);
+int search(int **array, int position, int query);
 
 //free the table
 void freeHashTable(struct node **array, int bucketNumber);
