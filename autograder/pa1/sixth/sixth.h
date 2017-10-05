a175ac7d98395de6d7d16cd937a022d1544a71e3
Kelvin <kelvinliu1234@gmail.com>
2017-10-05 15:11:02 -0400

    sixth

--- autograder/pa1/sixth/sixth.h
+++ autograder/pa1/sixth/sixth.h
@@ -5,6 +5,6 @@ int size;
 //check that string is only letters
 int validString(char *str);
 
-vvoid append(char *ans, char letter);
+void append(char *ans, char letter);
 	
 void appendNum(char *ans, char *number);
