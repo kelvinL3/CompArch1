7b545a8fe376aa77eb800bf5ce1654fd22832286
Kelvin <kelvinliu1234@gmail.com>
2017-10-05 11:56:57 -0400

    sixth

--- autograder/pa1/sixth/sixth.c
+++ autograder/pa1/sixth/sixth.c
@@ -14,13 +14,13 @@ int main(int argc, char **argv) {
 	char *ptr2 = ptr1+1;
 	
 	
-	for (ptr1=argv[1]; ptr1<size; ptr1++) {
+	for (ptr1=argv[1]; ptr1!='\0'; ptr1++) {
 		printf("%c\n", *ptr1);
-		for (ptr2=ptr1+1; ptr2<size; ptr2++) {
+		for (ptr2=ptr1+1; ptr2!='\0'; ptr2++) {
 			printf("%c\n", *ptr2);
 		}
 		printf("\n");
 	}
 	
-	
+	return 0;
 }
